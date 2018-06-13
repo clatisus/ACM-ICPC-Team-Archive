762db78c39f44a9aa05634a461e0c47b6347b482
zhongzihao <642524394@qq.com>
2018-06-13 23:15:50 +0800

    add codeforces 335

---
+++ zhongzihao-personal/codeforces/335/data.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+
+int main(){
+    int n = 100000;
+    printf("%d\n", n);
+    for (int i = 0; i < n; ++ i){
+        printf("%d %d %d %d\n", i, i, i + 1, i + 1);
+    }
+    return 0;
+}
