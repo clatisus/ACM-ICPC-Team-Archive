5b3531593602aa24cd0db35cf4bbba7af480e9c9
zhongzihao <642524394@qq.com>
2018-07-01 02:03:27 +0800

    add a special case

--- zhongzihao-personal/templates/source/number_theory/similar_euclid.cpp
+++ zhongzihao-personal/templates/source/number_theory/similar_euclid.cpp
@@ -79,7 +79,7 @@ int calc(int a, int b, int c, int n, int t1, int t2, int type, int sit){
         return 0;
     }
     if (!a){
-        return calcdp = 1ll * powermod(b / c, t2) * hvalue(t1, n) % moder;
+        return calcdp = 1ll * powermod(b / c, t2) * (hvalue(t1, n) + (t1 == 0)) % moder;
     }
     if (type == 0){
         if (a >= c || b >= c){
@@ -109,9 +109,33 @@ int calc(int a, int b, int c, int n, int t1, int t2, int type, int sit){
     return calcdp = ret;
 }
 
+int calc(int a, int b, int c, int n){ //special case for t1 = 0, t2 = 1
+    ll m = (1ll * a * n + b) / c;
+    if (!m){
+        return 0;
+    }
+    if (!a){
+        return 1ll * (b / c) * (n + 1) % moder;
+    }
+    if (a >= c || b >= c){
+        int ret = (1ll * n * (n + 1) >> 1) % moder * (a / c) % moder;
+        ret = (ret + 1ll * (n + 1) * (b / c)) % moder;
+        ret += calc(a % c, b % c, c, n);
+        ret -= ret >= moder ? moder : 0;
+        return ret;
+    }
+    int ret = (1ll * n * m - calc(c, c - b - 1, a, m - 1)) % moder;
+    ret += ret < 0 ? moder : 0;
+    return ret;
+}
+
 int solve(int a, int b, int c, int n, int t1, int t2){
     memset(dp, -1, sizeof(dp));
-    return calc(a, b, c, n, t1, t2, 0, 0);
+    int ret = calc(a, b, c, n, t1, t2, 0, 0);
+    if (t1 == 0 && t2 == 1){
+        assert(ret = calc(a, b, c, n));
+    }
+    return ret;
 }
 
 int main(){
