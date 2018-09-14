3a7787d1c5b0aed3590e1ea8ed9ba498d1f57135
ShinriiTin <shinrii.tin@aliyun.com>
2018-09-14 21:44:59 +0800

    add tarjan bcc edge stack

--- template/Graph_Theory/Tarjan_Algo/cut&bcc.cpp
+++ template/Graph_Theory/Tarjan_Algo/cut&bcc.cpp
@@ -39,3 +39,52 @@ namespace BCC {
         }
     }
 };
+
+// 有时候需要找出点双中的所有边, 可以使用下面的写法, 增加维护边的栈
+// 下面为求所有恰好只出现在一个简单环中的所有边的代码
+/*
+namespace BCC {
+    const int max_N = (int) 1e5 + 21;
+    const int max_M = max_N * 21;
+
+    int n, m, dfn[max_N], low[max_N], cnt, Pst[max_N], Ptop, Est[max_M], Etop, ans;
+
+    std::vector<std::pair<int, int>> vec[max_N]; // first : v, second : id
+
+    bool ret[max_M];
+
+    void tarjan(int u, int pre) {
+        dfn[u] = low[u] = ++cnt;
+        Pst[++Ptop] = u;
+        for (auto e : vec[u]) {
+            int v = e.first, w = e.second;
+            if (w == pre) continue;
+            if (!dfn[v]) {
+                Est[++Etop] = w;
+                tarjan(v, w);
+                low[u] = std::min(low[u], low[v]);
+                if (dfn[u] <= low[v]) {
+                    int Psiz = 1, Esiz = 0;
+                    do {
+                        ++Psiz;
+                    } while (Pst[Ptop--] != v);
+                    std::vector<int> tmp;
+                    do {
+                        ++Esiz;
+                        tmp.push_back(Est[Etop]);
+                    } while (Est[Etop--] != w);
+                    if (Psiz == Esiz) {
+                        for (auto i : tmp) {
+                            ++ans;
+                            ret[i] = true;
+                        }
+                    }
+                }
+            } else if (dfn[v] < dfn[u]) {
+                Est[++Etop] = w;
+                low[u] = std::min(low[u], dfn[v]);
+            }
+        }
+    }
+};
+*/
