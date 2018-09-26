7b2e2fc3cd6de9f35acb4f47beb2afe6d36b442b
zhongzihao <642524394@qq.com>
2018-09-26 10:04:57 +0800

    remove tabs

--- template/Number_Theory/BSGS/bsgs.cpp
+++ template/Number_Theory/BSGS/bsgs.cpp
@@ -9,67 +9,67 @@ typedef long long ll;
 
 class bsgs{
 private:
-	static std::vector <std::pair <int, int>> vec;
-	
-	int ans;
-	
-	static int powermod(int a, int exp, int moder){
-		int ret = 1;
-		for ( ; exp; exp >>= 1){
-			if (exp & 1){
-				ret = 1ll * ret * a % moder;
-			}
-			a = 1ll * a * a % moder;
-		}
-		return ret;
-	}
-	
-	// moder 为质数时可直接使用 
-	int BSGS(int a, int b, int moder){
-		a = (a % moder + moder) % moder;
-		b = (b % moder + moder) % moder;
-		if (!a && !b) return 0;
-		if (!a || !b) return -1;
-		vec.clear();
-		int now = b, x = a, m = std::sqrt(moder) + 1;
-		for (int i = 0; i < m; ++ i, now = 1ll * now * x % moder){
-			vec.push_back({now, -i});
-		}
-		std::sort(vec.begin(), vec.end());
-		x = now = powermod(a, m, moder);
-		for (int i = 1; i <= m; ++ i, now = 1ll * now * x % moder){
-			auto v = std::lower_bound(vec.begin(), vec.end(), std::make_pair(now, -INT_MAX));
-			if (v != vec.end() && v -> first == now) return i * m + v -> second;
-		}
-		return -1;
-	}
-	
-	int Bsgs(int a, int b, int moder){
-		int cnt = 0;
-		for (int gcd; (gcd = std::__gcd(a, moder)) != 1; ++ cnt){
-			if (b % gcd) return -1;
-			moder /= gcd;
-			b = 1ll * b / gcd * inv <int>(a / gcd, moder) % moder;
-			if (b == 1)	return cnt + 1;
-		}
-		int ret = BSGS(a, b, moder);
-		return !~ret ? -1 : ret + cnt;
-	}
-	
+    static std::vector <std::pair <int, int>> vec;
+    
+    int ans;
+    
+    static int powermod(int a, int exp, int moder){
+        int ret = 1;
+        for ( ; exp; exp >>= 1){
+            if (exp & 1){
+                ret = 1ll * ret * a % moder;
+            }
+            a = 1ll * a * a % moder;
+        }
+        return ret;
+    }
+    
+    // moder 为质数时可直接使用 
+    int BSGS(int a, int b, int moder){
+        a = (a % moder + moder) % moder;
+        b = (b % moder + moder) % moder;
+        if (!a && !b) return 0;
+        if (!a || !b) return -1;
+        vec.clear();
+        int now = b, x = a, m = std::sqrt(moder) + 1;
+        for (int i = 0; i < m; ++ i, now = 1ll * now * x % moder){
+            vec.push_back({now, -i});
+        }
+        std::sort(vec.begin(), vec.end());
+        x = now = powermod(a, m, moder);
+        for (int i = 1; i <= m; ++ i, now = 1ll * now * x % moder){
+            auto v = std::lower_bound(vec.begin(), vec.end(), std::make_pair(now, -INT_MAX));
+            if (v != vec.end() && v -> first == now) return i * m + v -> second;
+        }
+        return -1;
+    }
+    
+    int Bsgs(int a, int b, int moder){
+        int cnt = 0;
+        for (int gcd; (gcd = std::__gcd(a, moder)) != 1; ++ cnt){
+            if (b % gcd) return -1;
+            moder /= gcd;
+            b = 1ll * b / gcd * inv <int>(a / gcd, moder) % moder;
+            if (b == 1)    return cnt + 1;
+        }
+        int ret = BSGS(a, b, moder);
+        return !~ret ? -1 : ret + cnt;
+    }
+    
 public:
-	bsgs(){}
-	
-	bsgs(int a, int b, int moder){
-		ans = Bsgs(a, b, moder);
-	}
-	
-	int solve(){
-		return ans;
-	}
+    bsgs(){}
+    
+    bsgs(int a, int b, int moder){
+        ans = Bsgs(a, b, moder);
+    }
+    
+    int solve(){
+        return ans;
+    }
 };
 
 std::vector <std::pair <int, int>> bsgs::vec;
 
 int main(){
-	return 0;
+    return 0;
 }
--- template/Number_Theory/Primitive_Root/primitiveroot.cpp
+++ template/Number_Theory/Primitive_Root/primitiveroot.cpp
@@ -8,43 +8,43 @@
 template <typename T>
 class root{
 private:
-	T a, ans;
-	std::vector <T> fact;
-	
-	void solve(){
-		pollardrho <T> p(a - 1);
-		fact = p.solve();
-		std::sort(fact.begin(), fact.end());
-		fact.erase(std::unique(fact.begin(), fact.end()), fact.end());
-		for (T i = 1; i < a; ++ i){
-			if (checkroot(i)){
-				ans = i;
-				return;
-			}
-		}
-	}
+    T a, ans;
+    std::vector <T> fact;
+    
+    void solve(){
+        pollardrho <T> p(a - 1);
+        fact = p.solve();
+        std::sort(fact.begin(), fact.end());
+        fact.erase(std::unique(fact.begin(), fact.end()), fact.end());
+        for (T i = 1; i < a; ++ i){
+            if (checkroot(i)){
+                ans = i;
+                return;
+            }
+        }
+    }
 
 public:
-	root(){}
-	
-	root(T a):a(a){
-		solve();
-	}
-	
-	bool checkroot(T rt){
-		for (auto u : fact){
-			if (powermod(rt, a / u, a) == 1) return false;
-		}
-		return true;
-	}
-	
-	T minroot(){
-		return ans;
-	}
+    root(){}
+    
+    root(T a):a(a){
+        solve();
+    }
+    
+    bool checkroot(T rt){
+        for (auto u : fact){
+            if (powermod(rt, a / u, a) == 1) return false;
+        }
+        return true;
+    }
+    
+    T minroot(){
+        return ans;
+    }
 };
 
 int main(){
-	srand((unsigned) time(NULL));
-	printf("%d\n", root <int>(1000210433).minroot());
-	return 0;
+    srand((unsigned) time(NULL));
+    printf("%d\n", root <int>(1000210433).minroot());
+    return 0;
 }
