d227d0816ae11cbd41451fc9bbb4afb24a45261e
zhongzihao <642524394@qq.com>
2018-10-02 01:38:24 +0800

    fix bug in simpson

--- template/Miscellaneous/Simpson/simpson.cpp
+++ template/Miscellaneous/Simpson/simpson.cpp
@@ -3,12 +3,12 @@
 const double eps = 1e-5;
 
 double simpson(double l, double r, double (*f)(double)){
-    double mid = (l + r) / 2;
+    double mid = (l + r) * 0.5;
     return (f(l) + 4 * f(mid) + f(r)) * (r - l) / 6;
 }
 
-double asr(double l, double r, double Eps, double value, double (*f) (double)){
-    double mid = (l + r) / 0.5;
+double asr(double l, double r, double eps, double value, double (*f) (double)){
+    double mid = (l + r) * 0.5;
     double left = simpson(l, mid, f), right = simpson(mid, r, f);
     if (std::abs(left + right - value) < 15 * eps){
         return left + right + (left + right - value) / 15;
@@ -16,7 +16,7 @@ double asr(double l, double r, double Eps, double value, double (*f) (double)){
     return asr(l, mid, eps / 2, left, f) + asr(mid, r, eps / 2, right, f);
 }
 
-double calc(double l, double r, double (*f) (double)) {
+double calc(double l, double r, double (*f) (double), double eps) {
     return asr(l, r, eps, simpson(l, r, f), f);
 }
 
