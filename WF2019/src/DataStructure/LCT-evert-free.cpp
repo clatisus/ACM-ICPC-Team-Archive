7501eea7458f44b15191520e2dc3366e6da40d0d
coldwater <clatisus@gmail.com>
2019-03-21 15:34:27 +0800

    data structure tab

--- WF2019/src/DataStructure/LCT-evert-free.cpp
+++ WF2019/src/DataStructure/LCT-evert-free.cpp
@@ -3,72 +3,72 @@
  * Time: $O(n\log{n})$
  */
 namespace lct {
-    using star = struct node *;
+	using star = struct node *;
 #define Siz(x) ((x) ? (x)->siz : 0)
-    struct node {
-        int siz;
-        star p, ch[2];
-        void update() {
-            siz = Siz(ch[0]) + 1 + Siz(ch[1]);
-        }
-        int dir() {
-            if (!p)return -1;
-            if (p->ch[0] == this)return 0;
-            if (p->ch[1] == this)return 1;
-            return -1;
-        }
-        void set(star x, int d) {
-            if (~d && this)ch[d] = x;
-            if (x)x->p = this;
-        }
-    } pool[max_N], *tail = pool;
-    star new_node() {
-        star x = tail++;
-        x->siz = 1;
-        x->ch[0] = x->ch[1] = x->p = 0;
-        return x;
-    }
-    void rot(star x) {
-        star y = x->p;
-        int d = x->dir(), d1 = y->dir();
-        y->p->set(x, d1);
-        y->set(x->ch[d ^ 1], d), y->update();
-        x->set(y, d ^ 1);
-    }
-    void splay(star x) {
-        for (int d, d1; ~(d = x->dir()); rot(x))
-            if (~(d1 = x->p->dir())) {
-                rot((d ^ d1) ? x : x->p);
-            }
-        x->update();
-    }
-    void access(star x) {
-        star rch = 0;
-        for (star y = x; y; rch = y, y = y->p) {
-            splay(y), y->set(rch, 1), y->update();
-        }
-        splay(x);
-    }
-    void link(star x, star y) {//p[x]=y
-        access(x), assert(!x->p), x->p = y;
-    }
-    void cut(star x, star y) {//p[x]=y
-        access(x);
-        x->ch[0]->p = 0;
-        x->ch[0] = 0;
-        x->update();
-    }
-    int kth(star x, int k) {
-        while (x) {
-            int rk = Siz(x->ch[0]) + 1;
-            if (k == rk)break;
-            if (k < rk)x = x->ch[0];
-            else k -= rk, x = x->ch[1];
-        }
-        splay(x);
-        return x - pool;
-    }
-    int find(star x) {
-        return access(x), kth(x, 1);
-    }
+	struct node {
+		int siz;
+		star p, ch[2];
+		void update() {
+			siz = Siz(ch[0]) + 1 + Siz(ch[1]);
+		}
+		int dir() {
+			if (!p)return -1;
+			if (p->ch[0] == this)return 0;
+			if (p->ch[1] == this)return 1;
+			return -1;
+		}
+		void set(star x, int d) {
+			if (~d && this)ch[d] = x;
+			if (x)x->p = this;
+		}
+	} pool[max_N], *tail = pool;
+	star new_node() {
+		star x = tail++;
+		x->siz = 1;
+		x->ch[0] = x->ch[1] = x->p = 0;
+		return x;
+	}
+	void rot(star x) {
+		star y = x->p;
+		int d = x->dir(), d1 = y->dir();
+		y->p->set(x, d1);
+		y->set(x->ch[d ^ 1], d), y->update();
+		x->set(y, d ^ 1);
+	}
+	void splay(star x) {
+		for (int d, d1; ~(d = x->dir()); rot(x))
+			if (~(d1 = x->p->dir())) {
+				rot((d ^ d1) ? x : x->p);
+			}
+		x->update();
+	}
+	void access(star x) {
+		star rch = 0;
+		for (star y = x; y; rch = y, y = y->p) {
+			splay(y), y->set(rch, 1), y->update();
+		}
+		splay(x);
+	}
+	void link(star x, star y) {//p[x]=y
+		access(x), assert(!x->p), x->p = y;
+	}
+	void cut(star x, star y) {//p[x]=y
+		access(x);
+		x->ch[0]->p = 0;
+		x->ch[0] = 0;
+		x->update();
+	}
+	int kth(star x, int k) {
+		while (x) {
+			int rk = Siz(x->ch[0]) + 1;
+			if (k == rk)break;
+			if (k < rk)x = x->ch[0];
+			else k -= rk, x = x->ch[1];
+		}
+		splay(x);
+		return x - pool;
+	}
+	int find(star x) {
+		return access(x), kth(x, 1);
+	}
 };
--- WF2019/src/DataStructure/MiniTree.cpp
+++ WF2019/src/DataStructure/MiniTree.cpp
@@ -3,30 +3,30 @@
  * Time: $O(n\log{n})$
  */
 namespace mini_tree {
-    int st[max_N << 1], top;
-    struct functor {
-        bool operator()(int x, int y) const {
-            return dfnL[x] < dfnL[y];
-        }
-    };
-    inline bool isanc(int u, int v) {
-        return dfnL[u] <= dfnL[v] && dfnL[v] <= dfnR[u];
-    }
-    std::vector<std::pair<int, int>> _vec[max_N];
-    void build() {
-        std::sort(st + 1, st + 1 + top, functor());
-        int tmp = top;
-        for (int i = 1; i < tmp; ++i) {
-            st[++top] = lca(st[i], st[i + 1]);
-        }
-        std::sort(st + 1, st + 1 + top, functor());
-        tmp = std::unique(st + 1, st + 1 + top) - (st + 1);
-        top = 1;
-        for (int i = 2; i <= tmp; ++i) {
-            while (top && !isanc(st[top], st[i])) --top;
-            _vec[st[top]].emplace_back(
+	int st[max_N << 1], top;
+	struct functor {
+		bool operator()(int x, int y) const {
+			return dfnL[x] < dfnL[y];
+		}
+	};
+	inline bool isanc(int u, int v) {
+		return dfnL[u] <= dfnL[v] && dfnL[v] <= dfnR[u];
+	}
+	std::vector<std::pair<int, int>> _vec[max_N];
+	void build() {
+		std::sort(st + 1, st + 1 + top, functor());
+		int tmp = top;
+		for (int i = 1; i < tmp; ++i) {
+			st[++top] = lca(st[i], st[i + 1]);
+		}
+		std::sort(st + 1, st + 1 + top, functor());
+		tmp = std::unique(st + 1, st + 1 + top) - (st + 1);
+		top = 1;
+		for (int i = 2; i <= tmp; ++i) {
+			while (top && !isanc(st[top], st[i])) --top;
+			_vec[st[top]].emplace_back(
 				st[i], dep[st[i]] - dep[st[top]]);
-            st[++top] = st[i];
-        }
-    }
+			st[++top] = st[i];
+		}
+	}
 };
