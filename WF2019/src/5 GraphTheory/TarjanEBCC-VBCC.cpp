876c95704fc9b3b479db3af822117ba87167d9a6
coldwater <clatisus@gmail.com>
2019-03-22 18:26:22 +0800

    tarjan

---
+++ WF2019/src/5 GraphTheory/TarjanEBCC-VBCC.cpp
@@ -0,0 +1,48 @@
+<Tex> Tarjan EBCC and VBCC：每个VBCC新建点tot, tot的父亲为VBCC中深度最小的点u, 其余点v的父亲为tot.原图中每个点的信息维护在树中对应点和其父亲节点处, 若路径lca为新建点, 其父亲的信息也要考虑 </Tex>
+namespace Tarjan {
+    int dfn[max_N], low[max_N], cnt;
+    int cut[max_N], par[max_N * 2];
+    int st[max_N], top, tot;
+    int ebccno[max_N], ebcc_cnt;
+    std::vector<int> vec[max_N], ebcc[max_N], bcc[max_N * 2];
+    void tarjan(int u, int p) {
+        dfn[u] = low[u] = ++cnt;
+        st[++top] = u;
+        for (auto &v : vec[u]) {
+            if (v == p) continue;
+            if (!dfn[v]) {
+                tarjan(v, u);
+                low[u] = std::min(low[u], low[v]);
+                //bridge(u, v) = dfn[u] < low[v] && !multiEdge(u, v);
+                if (dfn[u] <= low[v]) {
+                    cut[u] = 1;
+                    int x = -1;
+                    par[++tot] = u;
+                    bcc[u].push_back(tot);
+                    while (x != v) {
+                        x = st[top--];
+                        bcc[tot].push_back(x);
+                        par[x] = tot;
+                    }
+                }
+            } else if (dfn[v] < dfn[u]) {
+                low[u] = std::min(low[u], dfn[v]);
+            }
+        }
+        if (!p && vec[u].size() <= 1) cut[u] = 0;
+        /*if (dfn[u] == low[u]) {
+            ++ebcc_cnt;
+            int v = -1;
+            while (u != v) {
+                ebccno[v = st[top--]] = ebcc_cnt;
+                ebcc[ebcc_cnt].push_back(v);
+            }
+        }*/
+    }
+    void solve(int n) {
+        tot = n;
+        for (int i = 1; i <= n; ++i) {
+            if (!dfn[i]) tarjan(i, i);
+        }
+    }
+}
