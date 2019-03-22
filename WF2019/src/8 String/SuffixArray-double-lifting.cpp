402265a029fb1a123fe0a9b05b5a798b688e43c6
coldwater <clatisus@gmail.com>
2019-03-23 01:31:14 +0800

    fix

--- WF2019/src/8 String/SuffixArray-double-lifting.cpp
+++ WF2019/src/8 String/SuffixArray-double-lifting.cpp
@@ -4,28 +4,25 @@ namespace SuffixArray { // 字符串从1开始
 	inline void build(const char *s, int n) {
 		int i, j, k, T, sigma;
 		for (i = 1; i <= n; ++i) sa[i] = i;
-		std::sort(sa+1, sa+1+n, [&](int a, int b) {return s[a] < s[b];});
-		for (i = 2, bc[sa[1]] = 1; i <= n; ++i) {
+		sort(sa+1,sa+1+n,[&](int a,int b){return s[a]<s[b];});
+		for (i = 2, bc[sa[1]] = 1; i <= n; ++i)
 			bc[sa[i]] = bc[sa[i-1]] + (s[sa[i-1]] < s[sa[i]]);
-		}
 		for (T = 1; (sigma = bc[sa[n]]) < n; T <<= 1) {
 			auto cmp = [&](const int &a, const int &b) {
 				if (bc[a] != bc[b]) return bc[a] < bc[b];
-				return (a+T>n || b+T> n) ? a > b : bc[a+T] < bc[b+T];
+				return (a+T>n||b+T>n)?a>b:bc[a+T] < bc[b+T];
 			};
 			for (i = 0; i <= sigma; ++i) cnt[i] = 0;
-			for (i = 1; i <= n; ++i) ++cnt[(i+T>n) ? 0 : bc[i+T]];
+			for (i = 1; i <= n; ++i) ++cnt[(i+T>n)?0:bc[i+T]];
 			for (i = 1; i <= sigma; ++i) cnt[i] += cnt[i - 1];
-			for (i = n; i; --i) {
-				tmp[cnt[(sa[i]+T > n) ? 0 : bc[sa[i]+T]]--] = sa[i];
-			}
+			for (i = n; i; --i)
+				tmp[cnt[(sa[i]+T>n)?0:bc[sa[i]+T]]--] = sa[i];
 			for (i = 0; i <= sigma; ++i) cnt[i] = 0;
 			for (i = 1; i <= n; ++i) ++cnt[bc[i]];
 			for (i = 1; i <= sigma; ++i) cnt[i] += cnt[i - 1];
 			for (i = n; i; --i) sa[cnt[bc[tmp[i]]]--] = tmp[i];
-			for (i = 2, tmp[sa[1]] = 1; i <= n; ++i) {
+			for (i = 2, tmp[sa[1]] = 1; i <= n; ++i)
 				tmp[sa[i]] = tmp[sa[i-1]] + cmp(sa[i-1], sa[i]);
-			}
 			memcpy(bc + 1, tmp + 1, sizeof(int) * n);
 		}
 		for (i = 1; i <= n; ++i) rk[sa[i]] = i;
@@ -33,9 +30,7 @@ namespace SuffixArray { // 字符串从1开始
 			if (rk[i] == 1) k = 0;
 			else {
 				j = sa[rk[i] - 1]; if (k) --k;
-				while (i+k <= n && j+k <= n && s[i+k] == s[j+k]) ++k;
+				while(i+k<=n && j+k<=n && s[i+k]==s[j+k]) ++k;
 			}
 			height[rk[i]] = k;
-		}
-	}
-};
+		}}};
--- WF2019/src/8 String/manacher.cpp
+++ WF2019/src/8 String/manacher.cpp
@@ -8,10 +8,8 @@ namespace Manacher {
 		register int i, j, k;
 		int *R = r[type];
 		for(i=j=0; i<len; i+=k, j=std::max(j-k,0)){
-			while(i-j>=0 && i+j+type<len && S[i-j]==S[i+j+type]) ++j;
+			while(i-j>=0&&i+j+type<len&&S[i-j]==S[i+j+type])++j;
 			R[i] = j;
 			for (k = 1; k < j && R[i - k] != R[i] - k; ++k)
 				R[i + k] = std::min(R[i - k], R[i] - k);
-		}
-	}
-};
+		}}};
