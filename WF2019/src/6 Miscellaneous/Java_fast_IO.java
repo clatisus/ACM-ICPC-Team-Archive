3ed764f9d57ee50aa5ed508bf81f30419f4b0e5e
coldwater <clatisus@gmail.com>
2019-03-22 15:59:06 +0800

    java io

--- WF2019/src/6 Miscellaneous/Java_fast_IO.java
+++ WF2019/src/6 Miscellaneous/Java_fast_IO.java
@@ -1,16 +1,9 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class FastIO {
     public static void main(String[] args){
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         try {
             String string = reader.readLine();
-            System.out.println(string);
-        }
-        catch (IOException e){
-            throw new RuntimeException(e);
-        }
+            System.out.println(string);}
+        catch (IOException e){throw new RuntimeException(e);}
     }
 }
