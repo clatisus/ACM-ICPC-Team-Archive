580bd55536f0809c7b727b4c305c233ae03509ee
coldwater <clatisus@gmail.com>
2019-03-22 18:52:13 +0800

    data

--- WF2019/src/0 Data Structure/CartesianTree.cpp
+++ WF2019/src/0 Data Structure/CartesianTree.cpp
@@ -1,4 +1,4 @@
-// (a[], i), root is st[1]
+<TeX>(a[], i), root is st[1]</TeX>
 for (int i = 1; i <= n; ++i) {
 	while (top && a[st[top]] >= a[i]) ch[i][0] = st[top--];
 	if (top) ch[st[top]][1] = i;
