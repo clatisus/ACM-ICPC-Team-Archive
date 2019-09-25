4f49497139bbd4acca9acd2003d44624cb13caa5
zhongzihao <642524394@qq.com>
2019-09-26 00:25:50 +0800

    update min25 sieve

--- WF2019/src/7 Number Theory/Min25Sieve.cpp
+++ WF2019/src/7 Number Theory/Min25Sieve.cpp
@@ -1,62 +1,79 @@
-<TeX>这份模板中，$f$ 和 $g$ 分别是 $0$ 次函数和 $1$ 次函数。这里 $N$ 要尽量开大，至少要比 $\sqrt{n}$ 后的一个质数大</TeX>
+#include <bits/stdc++.h>
+
+typedef long long ll;
+const int N = 40000010; // $N$ 要比 $\sqrt{n}$ 后的一个质数大
+const int M = N << 1;
+
 int min[N];
-std::vector <int> pri, preg;
-void init(){
-	preg.push_back(1);
-	for (int i = 2; i < N; ++ i){
-		if (!min[i]){
-			min[i] = i; pri.push_back(i);
-			preg.push_back((preg.back() + i) % moder);
-		}
-		for(int j=0,sz=pri.size();j<sz&&i*prime[j]<N; ++j){
-			min[i * pri[j]] = prime[j];
-			if (i % pri[j] == 0) break;
-		}}}
-int f[N], g[N], sqroot;
+std::vector<int> pri;
+std::vector<ll> pre; // 所求函数 1 及质数处的前缀和
+ll f[M];
 ll n;
-inline int sum0(ll n){return n % moder;}
-inline int sum1(ll n){n %= moder; return n * (n + 1) % moder * inv2 % moder;}
-inline int getsit(ll x){
-	if (x > INT_MAX) return sqroot + n / x;
-	return x * x > n ? sqroot + n / x : x;
+
+void init() {
+    pre.push_back(1);
+    for (int i = 2; i < N; ++i) {
+        if (!min[i]) {
+            min[i] = i;
+            pri.push_back(i);
+            pre.push_back(pre.back() + 2);
+        }
+        for (int j = 0, sz = pri.size(); j < sz && i * pri[j] < N; ++j) {
+            min[i * pri[j]] = pri[j];
+            if (i % pri[j] == 0) break;
+        }
+    }
 }
-void calcf(){
-	sqroot = std::sqrt(n);
-	std::vector <ll> vec; std::vector <int> id;
-	for (ll i = 1, j; i <= n; i = j + 1) {
-		ll x = n / i;
-		int sit = getsit(x); j = n / x;
-		f[sit] = sum0(x), g[sit] = sum1(x);
-		vec.push_back(x); id.push_back(sit);
-	}
-	for (int k = 0, szk = pri.size(); k < szk; ++k) {
-		int p = pri[k];
-		if (1ll * p * p > n) break;
-		for (int i = 0, szi = vec.size(); i < szi; ++i) {
-			ll x = vec[i];
-			if (x < 1ll * p * p) break;
-			int sit = getsit(x / p);
-			int &u = f[id[i]];
-			u = (u - f[sit] + k + 1) % moder;
-			u += u < 0 ? moder : 0;
-			int &v = g[id[i]];
-			v = (v - 1ll * p * (g[sit] - preg[k])) % moder;
-			v += v < 0 ? moder : 0;
-		} 
-	}
+
+inline int getsit(ll x) {
+    return x ? x <= n / x ? x : M - n / x : 0;
 }
-int calcsum(ll n, int sit){
-	if (pri[sit] > n) return 0;
-	int id = getsit(n);
-	int ret = (g[id]-f[id] - (preg[sit]-sit-1)) % moder;
-	if (sit == 0) ret += 2;
-	for (int i = sit, sz = pri.size(); i < sz; ++ i){
-		int p = pri[i];
-		ll now1 = p, now2 = 1ll * p * p;
-		if (now2 > n) break;
-		for(int j = 1; now2 <= n; ++j,now1=now2,now2*=p)
-			ret = (ret + 1ll * (p ^ j) * calcsum(n / now1, i + 1) + (p ^ (j + 1))) % moder;
-	}
-	ret += ret < 0 ? moder : 0;
-	return ret;
+
+ll calcf(int p, int j){
+    ll sum = 0, now = 1;
+    for (int i = 0; i <= j; ++ i){
+        sum += now;
+        if (i & 1){
+            now *= p;
+        }
+    }
+    return sum;
+}
+
+void calcpri() {
+    std::vector<ll> vec;
+    std::vector<int> id;
+    for (ll i = 1, j; i <= n; i = j + 1) {
+        ll x = n / i;
+        int sit = getsit(x);
+        j = n / x;
+        f[sit] = sum(x); // 质数处的函数值要求是个多项式，这里是将该多项式推广到 Z 上的前缀和
+        vec.push_back(x);
+        id.push_back(sit);
+    }
+    for (int k = 0, szk = pri.size(); k < szk; ++k) {
+        int p = pri[k];
+        if (1ll * p * p > n) break;
+        for (int i = 0, szi = vec.size(); i < szi; ++i) {
+            ll x = vec[i];
+            if (x < 1ll * p * p) break;
+            int sit = getsit(x / p);
+            f[id[i]] -= f[sit] - pre[k];
+        }
+    }
+}
+
+ll calcsum(ll n, int sit) {
+    if (pri[sit] > n) return 0;
+    int id = getsit(n);
+    ll ret = f[id] - pre[sit]; // >=p[sit] 的质数处和
+    for (int i = sit, sz = pri.size(); i < sz; ++i) { // 枚举最小质因子
+        int p = pri[i];
+        ll now1 = p, now2 = 1ll * p * p;
+        if (now2 > n) break;
+        for (int j = 1; now2 <= n; ++j, now1 = now2, now2 *= p){
+            ret += calcf(p, j) * calcsum(n / now1, i + 1) + calcf(p, j + 1);
+        }
+    }
+    return ret;
 }
