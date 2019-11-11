1a9085da3d432caad6f026e9904bb0c3a52136b4
zhongzihao <642524394@qq.com>
2019-11-11 23:29:11 +0800

    add cf 411

--- zhongzihao-personal/codeforces/411/411-1-A.cpp
+++
@@ -1,8 +0,0 @@
-#include<bits/stdc++.h>
-
-int n;
-
-int main(){
-	scanf("%d", &n);
-	return printf("%d\n", n & 1 ? n >> 1 : (n >> 1) - 1), 0;
-}
--- zhongzihao-personal/codeforces/411/411-2-A.cpp
+++
@@ -1,7 +0,0 @@
-#include<bits/stdc++.h>
-
-int main(){
-	int l, r;
-	scanf("%d%d", &l, &r);
-	return printf("%d\n", l == r ? l : 2), 0;
-}
